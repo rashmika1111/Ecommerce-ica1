0126cf4b409550abfe54dacc0db72307a9a74878
rashmika1111 <139681232+rashmika1111@users.noreply.github.com>
2025-05-08 03:45:10 +0530

    models created

---
+++ ica1/src/main/java/com/example/demo/Ica1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ica1Application.class, args);
+	}
+
+}
